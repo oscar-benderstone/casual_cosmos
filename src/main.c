6ee47b214f85c985c030c555955115a2e701b1bf
oscar-benderstone <oscarbenderstone@gmail.com>
2025-07-03 20:21:58 -0600

    Setup extra SDL libraries in cmake and added clangd flags

--- src/main.c
+++ src/main.c
@@ -1,8 +1,10 @@
 // SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
-
 #include "app.h"
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_log.h>
+// #include <SDL3_image/SDL_image.h>
+// #include <SDL3_mixer/SDL_mixer.h>
+// #include <SDL3_ttf/SDL_ttf.h>
 
 int main(int argc, char *argv[]) {
   static enum App_Status app_status = APP_CONTINUE;
