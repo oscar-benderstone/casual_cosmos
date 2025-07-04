9ef2af204d55731498cbc1f84cfa34218be08c33
oscar-benderstone <oscarbenderstone@gmail.com>
2025-07-03 19:11:07 -0600

    Added includes

---
+++ include/app.h
@@ -0,0 +1,6 @@
+#ifndef APP_STATUS
+#define APP_STATUS
+
+enum App_Status { APP_SUCCESS = 0, APP_FAILURE = 1, APP_CONTINUE };
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,8 +1,8 @@
+// SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+#include "app.h"
 #include <SDL3/SDL_init.h>
 #include <SDL3/SDL_log.h>
-#include <SDL3/SDL_oldnames.h>
-
-enum App_Status { APP_SUCCESS = 0, APP_FAILURE = 1, APP_CONTINUE };
 
 int main(int argc, char *argv[]) {
   static enum App_Status app_status = APP_CONTINUE;
